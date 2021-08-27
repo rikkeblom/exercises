1312b0b036ae1d273b0e8c3d8b974c855e12e204
Rikke Blom <rikk957v@stud.kea.dk>
2021-08-27 18:28:36 +0200

    updating and adding current pages

---
+++ class-exercises/datatypes.js
@@ -0,0 +1,15 @@
+"use strict";
+console.log("Hello World");
+const bool = true;
+const num = 41;
+const str = "Peter";
+const obj = {
+  cats: 2,
+  cars: 1,
+};
+const nothing = null;
+let undf;
+const symbol = Symbol("symbol");
+
+//find the value of the constant or variable in the live-server console by typing the name and clicking enter
+//use "typeof" followed by the name in the console to see the datatype
---
+++ class-exercises/truthyorfalsy.js
@@ -0,0 +1,9 @@
+// console.log("testing truths");
+
+const value = " ";
+
+if (value) {
+  console.log("value is truthy");
+} else {
+  console.log("value is falsy");
+}
