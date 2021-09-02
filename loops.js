2d8f7191a4d14ccaad261fce23b6a9dda021a5ff
Rikke Blom <rikk957v@stud.kea.dk>
2021-09-02 11:42:19 +0200

    for loop exercise

---
+++ loops.js
@@ -0,0 +1,35 @@
+//console log 0-9
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+//console log 1-10
+// for (let i = 1; i < 11; i++) {
+//   console.log(i);
+// }
+
+//console log from 10-0, and after 0 it console logs "liftoff"
+// for (let i = 10; i > 0; i--) {
+//   console.log(i);
+// }
+// console.log("liftoff");
+
+//console log from 1-19 but only counts unevennumbers
+// for (let i = 1; i < 20; i += 2) {
+//   console.log(i);
+// }
+
+//console log from 1-16777216 but doubling each time
+// for (let i = 1; i <= 16777216; i *= 2) {
+//   console.log(i);
+// }
+
+//console log from 111-138 in steps of 3 (111, 114, 117..)
+// for (let i = 111; i <= 138; i += 3) {
+//   console.log(i);
+// }
+
+//console log from 100-0 in steps of 10
+for (let i = 100; i >= 0; i -= 10) {
+  console.log(i);
+}
